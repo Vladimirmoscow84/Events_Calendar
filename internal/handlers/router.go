7c35121c0296948e81c8fc13b6c805f08818715f
Vladimirmoscow84 <ccr1@yandex.ru>
2025-12-07 22:38:53 +0300

    add Router

---
+++ internal/handlers/router.go
@@ -0,0 +1,48 @@
+package handlers
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/Vladimirmoscow84/Events_Calendar/internal/model"
+	"github.com/gin-gonic/gin"
+)
+
+type eventsConstructor interface {
+	Create(ctx context.Context, event *model.Event) (int, error)
+	Update(ctx context.Context, event *model.Event) error
+	Delete(ctx context.Context, eventID int) error
+}
+
+type eventsGetter interface {
+	EventsForDay(ctx context.Context, userID int, day time.Time) ([]model.Event, error)
+	EventsForWeek(ctx context.Context, userID int, day time.Time) ([]model.Event, error)
+	EventsForMonth(ctx context.Context, userID int, day time.Time) ([]model.Event, error)
+}
+
+type Router struct {
+	Engine      *gin.Engine
+	Constructor eventsConstructor
+	Getter      eventsGetter
+}
+
+func New(e *gin.Engine, ctr eventsConstructor, gtr eventsGetter) (*Router, error) {
+	if e == nil || ctr == nil || gtr == nil {
+		return nil, errors.New("[handlers] invalid Router parametrs")
+	}
+	return &Router{
+		Engine:      e,
+		Constructor: ctr,
+		Getter:      gtr,
+	}, nil
+}
+
+func (r *Router) Routes() {
+	r.Engine.POST("/create_event", r.CreateEventHandler)
+	r.Engine.POST("/update_event", r.UpdateEventHandler)
+	r.Engine.POST("/delete_event", r.DeleteEventHandler)
+	r.Engine.GET("/events_for_day", r.EventsForDayHandler)
+	r.Engine.GET("/events_for_week", r.EventsForWeekHandler)
+	r.Engine.GET("/events_for_month", r.EventsForMonthHandler)
+}
